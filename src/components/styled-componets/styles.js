30475ac5fd4be3067cae186d03fe85abbb972b89
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-02-14 16:32:31 +0200

    Initialized app with create-react-app

---
+++ src/components/styled-componets/styles.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  max-width: 117rem;
+  margin: 0 auto;
+  padding: 0 1.6rem;
+`;
